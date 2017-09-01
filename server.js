105cb166a622a5dddc21594ed7f5aa731c437839
津崎 <i1658119@cse.kyoto-su.ac.jp>
2017-09-01 15:35:52 +0900

    webサーバーの雛形を作成

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+const port = 3000
+
+//静的ファイルの読み込み
+app.use(express.static(path.join(__dirname,'client')))
+
+app.listen(port,err => {
+    if(err) throw new Error(err)
+        else console.log(`port ${port}`)
+    }
+)
+
