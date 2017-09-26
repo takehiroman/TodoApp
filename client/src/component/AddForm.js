391666b686c21fade533c4d915c86a62ca6809d3
津崎 <i1658119@cse.kyoto-su.ac.jp>
2017-09-27 04:44:13 +0900

    post処置が完了した

--- client/src/component/AddForm.js
+++ client/src/component/AddForm.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { addTodo } from '../actions'
+import { addTodo,initializeForm } from '../actions'
+import axios from 'axios'
 
 const AddForm = ({ store }) => {
     //formからの内容を取得する
     const { todo } = store.getState().form
 
+    const handleSubmit = e => {
+        //formのsubmitした時のデフォルト動作を抑制
+        e.preventDefault()
+
+        axios.post('/api/todos',{
+            todo,
+        })
+        .then(response => {
+            console.log(response)
+            store.dispatch(initializeForm())
+        })
+        .catch(err => {
+            console.log(new Error(err))
+        })
+    }
+
     return(
         <div>
-            <form>
+            <form onSubmit={e => handleSubmit(e)}>
                 <label>
                     新しいTodo:
                     <input value={todo} onChange={e => store.dispatch(addTodo(e.target.value))} />
--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
+const mongoose = require('mongoose')
 const app = express();
+import Todo from './todo'
 const port = 3001
+
+const dbUrl = 'mongodb://localhost/todo'
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+mongoose.connect(dbUrl,dbErr => {
+    if(dbErr) throw new Error(dbErr)
+    else console.log('db connected')
+
+    app.post('/api/todos',(request,response) => {
+        const { todo } = request.body
+
+        new Todo({
+            todo,
+        }).save(err => {
+            if(err) response.status(500)
+                else response.status(200).send(`${todo} was succes.`)
+        })
+    })    
+
+    app.listen(port,err => {
+        if(err) throw new Error(err)
+        else console.log(`listen on port ${port}`)
+    })
+})
 
 //静的ファイルの読み込み
 app.use(express.static(path.join(__dirname,'client/build')))
 
-app.listen(port,err => {
-    if(err) throw new Error(err)
-        else console.log(`port ${port}`)
-    }
-)
 
