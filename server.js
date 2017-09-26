0bc65100a882418df206b2979cccae39323fb385
津崎 <i1658119@cse.kyoto-su.ac.jp>
2017-09-26 22:20:00 +0900

    クライアントサイドにReactの雛形を追加した

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000
 
 //静的ファイルの読み込み
-app.use(express.static(path.join(__dirname,'client')))
+app.use(express.static(path.join(__dirname,'client/build')))
 
 app.listen(port,err => {
     if(err) throw new Error(err)
